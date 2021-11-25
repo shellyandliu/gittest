bbc95bfdc981abe0ffeb4a54a4b45d37ec5e2ec3
Shelly <Shelly@qq.com>
2021-11-25 23:06:15 +0800

    version-1

---
+++ src/testgit/add.java
@@ -0,0 +1,11 @@
+package testgit;
+
+/**
+ * @author liuxiangyu
+ * @DATE 2021/11/25 - 23:03
+ */
+public class add {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
---
+++ src/testgit/test.java
@@ -0,0 +1,8 @@
+package testgit;
+
+/**
+ * @author liuxiangyu
+ * @DATE 2021/11/25 - 23:01
+ */
+public class test {
+}
